4d90cdf383c54820d5578c1776e1463cf63bc7f1
johnotieno0 <poptiff@gmail.com>
2024-09-19 17:10:00 +0300

    feat: declare flags using flag package

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// type Options struct {
+// 	l        bool
+// 	a        bool
+// 	progName string
+// 	path     string
+// }
+
+// var options Options
+
+func main() {
+	longFormat := flag.Bool("l", false, "Use long listing format.")
+	allFiles := flag.Bool("a", false, "Show hidden files.")
+	recursiveDir := flag.Bool("R", false, "List subdirectories recursively.")
+	timeFlag := flag.Bool("t", false, "List files in descending order of time (i.e. newest first)")
+	reverser := flag.Bool("r", false, "List in reverse order.")
+
+	flag.Parse()
+
+	fmt.Printf("Long format: %v\n", *longFormat)
+	fmt.Printf("Show all files: %v\n", *allFiles)
+	fmt.Printf("List subdirectories recursively: %v\n", *recursiveDir)
+	fmt.Printf("Order time: %v\n", *timeFlag)
+	fmt.Printf("Order in reverse: %v\n", *reverser)
+
+	args := flag.Args()
+	fmt.Printf("Other arguments: %v\n", args)
+	// options.progName = os.Args[0]
+	// args := os.Args[1:]
+	// for _, arg := range args {
+	// 	switch arg {
+	// 	case "-l":
+	// 		options.l = true
+	// 	case "-a":
+	// 		options.a = true
+	// 	}
+	// }
+	// fmt.Println(options)
+}
+
+// func ls(args []string) error {
+
+// }
