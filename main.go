e5151b3d7b1c08a9772a1f510648d7ee27cc8ded
johnotieno0 <poptiff@gmail.com>
2024-11-06 11:41:14 +0300

    fix: sort paths alphabetically - refactor main function to call sortPaths and print path before printing output from directory paths

--- main.go
+++ main.go
@@ -9,19 +9,20 @@ import (
 
 func main() {
 	// Parse flags from command line
-	flags, args := lsfunctions.ParseFlags(os.Args[1:])
-	if len(args) == 0 {
-		args = []string{"."}
+	flags, paths := lsfunctions.ParseFlags(os.Args[1:])
+	if len(paths) == 0 {
+		paths = []string{"."}
 	}
-	for i, path := range args {
-		if flags.Recursive || len(args) > 1 {
+	paths, idx := lsfunctions.SortPaths(paths)
+	for i, path := range paths {
+		if flags.Recursive && len(paths) > 1 || i > idx  {
 			fmt.Printf("%s:\n", path)
 		}
 		err := lsfunctions.ListPath(path, flags)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ls: %s: %v\n", path, err)
 		}
-		if i < len(args)-1 {
+		if i < len(paths)-1 {
 			fmt.Println()
 		} 
 	}
