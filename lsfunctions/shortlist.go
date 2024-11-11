2581b66eb3825d89f9119a50b1e7b36e43cd7411
johnotieno0 <poptiff@gmail.com>
2024-11-11 13:40:27 +0300

    refactor: create  new []Entry  and refactor GetShortFormat to use Entry refactor: GetShortFormat to call colorName function for coloring

--- lsfunctions/shortlist.go
+++ lsfunctions/shortlist.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 )
 
-func DisplayShortList(entries []FileInfo) {
+func DisplayShortList(e []FileInfo) {
+	// Process entries to create type []Entry
+	entries, _ := processEntries(e)
 	var fileNameEntries []string
 	displayColumn := false
 
@@ -63,48 +65,10 @@ func DisplayShortList(entries []FileInfo) {
 	}
 }
 
-func GetShortFormat(entry FileInfo) string {
-	name := entry.Name
-	info := entry.Info
-	// Add color blue for directories
-	if info.IsDir() {
-		return "\x1b[34m" + name + "\x1b[0m"
-	}
-	// Color coding based on file type
-	switch getFileTypeShort(entry) {
-	case "text":
-		name = "\x1b[97m" + name + "\x1b[0m" // White
-	case "pdf":
-		name = "\x1b[91m" + name + "\x1b[0m" // Light Red
-	case "word":
-		name = "\x1b[94m" + name + "\x1b[0m" // Light Blue
-	case "excel":
-		name = "\x1b[92m" + name + "\x1b[0m" // Light Green
-	case "powerpoint":
-		name = "\x1b[93m" + name + "\x1b[0m" // Light Yellow
-	case "archive":
-		name = "\x1b[31m" + name + "\x1b[0m" // Red
-	case "audio":
-		name = "\x1b[96m" + name + "\x1b[0m" // Light Cyan
-	case "video":
-		name = "\x1b[95m" + name + "\x1b[0m" // Light Magenta
-	case "image":
-		name = "\x1b[35m" + name + "\x1b[0m" // Magenta
-	case "go":
-		name = "\x1b[36m" + name + "\x1b[0m" // Cyan
-	case "python":
-		name = "\x1b[33m" + name + "\x1b[0m" // Yellow
-	case "javascript":
-		name = "\x1b[33m" + name + "\x1b[0m" // Yellow
-	case "html":
-		name = "\x1b[91m" + name + "\x1b[0m" // Light Red
-	case "css":
-		name = "\x1b[36m" + name + "\x1b[0m" // Cyan
-	case "link":
-		name = "\x1b[38;5;51m" + name + "\x1b[0m"
-	case "exec":
-		name = "\x1b[38;5;46m" + name + "\x1b[0m" // Add color green for executables
-	}
-	return name
+func GetShortFormat(entry Entry) string {
+	// Color output
+	entry = colorName(entry)
+
+	return entry.Name
 }
 
