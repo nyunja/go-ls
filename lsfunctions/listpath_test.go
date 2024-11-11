4decab70e0ce6f9f11d727a2fdb9720bfa34cff8
johnotieno0 <poptiff@gmail.com>
2024-11-11 23:30:59 +0300

    update: test for quickSort and compareEntries functions

--- lsfunctions/listpath_test.go
+++ lsfunctions/listpath_test.go
@@ -24,18 +24,6 @@ func (m mockFileInfo) ModTime() time.Time { return m.modTime }
 func (m mockFileInfo) IsDir() bool        { return m.mode.IsDir() }
 func (m mockFileInfo) Sys() interface{}   { return m.sys }
 
-// Mock a []FileInfo for testing
-var mockEntries = []FileInfo{
-	{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
-	{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
-	{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
-}
-var sortedEntries = []FileInfo{
-	{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
-	{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
-	{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
-}
-
 func TestSortPaths(t *testing.T) {
 	type args struct {
 		paths []string
@@ -123,6 +111,17 @@ func Test_cleanName(t *testing.T) {
 }
 
 func Test_sortEntries(t *testing.T) {
+	// Mock a []FileInfo for testing
+	var mockEntries = []FileInfo{
+		{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
+		{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
+		{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
+	}
+	var sortedEntries = []FileInfo{
+		{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
+		{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
+		{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
+	}
 	type args struct {
 		entries []FileInfo
 		flags   Flags
@@ -133,15 +132,80 @@ func Test_sortEntries(t *testing.T) {
 		want []FileInfo
 	}{
 		{name: "test 1", args: args{entries: mockEntries, flags: Flags{}}, want: sortedEntries},
-
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := sortEntries(tt.args.entries, tt.args.flags)
 			for i, entry := range got {
-				if!reflect.DeepEqual(entry.Name, tt.want[i].Name) {
-                    t.Errorf("sortEntries() entry.Name = %v, want %v", entry.Name, tt.want[i].Name)
-                }
+				if !reflect.DeepEqual(entry.Name, tt.want[i].Name) {
+					t.Errorf("sortEntries() entry.Name = %v, want %v", entry.Name, tt.want[i].Name)
+				}
+			}
+		})
+	}
+}
+
+func Test_quickSort(t *testing.T) {
+	// Mock a []FileInfo for testing
+	var mockEntries = []FileInfo{
+		{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
+		{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
+		{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
+	}
+	var sortedEntries = []FileInfo{
+		{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
+		{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
+		{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
+	}
+	type args struct {
+		entries []FileInfo
+		low     int
+		high    int
+		flags   Flags
+		want    []FileInfo
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{name: "test 1", args: args{entries: mockEntries, low: 0, high: len(mockEntries) - 1, flags: Flags{}, want: sortedEntries}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			quickSort(tt.args.entries, tt.args.low, tt.args.high, tt.args.flags)
+		})
+		for i, entry := range tt.args.entries {
+			if !reflect.DeepEqual(entry.Name, tt.args.want[i].Name) {
+				t.Errorf("sortEntries() entry.Name = %v, want %v", entry.Name, tt.args.want[i].Name)
+			}
+		}
+	}
+}
+
+func Test_compareEntries(t *testing.T) {
+	// Mock a []FileInfo for testing
+	var mockEntries = []FileInfo{
+		{Name: "main.go", Info: mockFileInfo{name: "main.go", mode: 0100644, modTime: time.Now()}},
+		{Name: "ted", Info: mockFileInfo{name: "ted", mode: 040755, modTime: time.Now()}},
+		{Name: "go.mod", Info: mockFileInfo{name: "go.mod", mode: 0644, modTime: time.Now()}},
+	}
+	type args struct {
+		a     FileInfo
+		b     FileInfo
+		flags Flags
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test 1", args: args{a: mockEntries[0], b: mockEntries[1], flags: Flags{}}, want: true},
+		{name: "test 2", args: args{a: mockEntries[0], b: mockEntries[2], flags: Flags{}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compareEntries(tt.args.a, tt.args.b, tt.args.flags); got != tt.want {
+				t.Errorf("compareEntries() = %v, want %v", got, tt.want)
 			}
 		})
 	}
