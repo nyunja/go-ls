4c477e8ac4ab2e536502b12c7fed7f119bb86044
johnotieno0 <poptiff@gmail.com>
2024-11-11 23:28:07 +0300

    org: remove unnecessary color types

--- lsfunctions/getfiletype.go
+++ lsfunctions/getfiletype.go
@@ -8,36 +8,22 @@ func getFileType(entry Entry) string {
 	if strings.ContainsAny(mod, "x") {
 		return "exec"
 	}
-	name := entry.Name
+	name := strings.Trim(entry.Name, "'")
 	tokens := strings.Split(strings.ToLower(name), ".")
 	ext := tokens[len(tokens)-1]
-	switch ext {
-	case ".txt", ".md", ".log":
+	switch "." + ext {
+	case ".log":
 		return "text"
 	case ".pdf":
 		return "pdf"
-	case ".doc", ".docx":
-		return "word"
-	case ".xls", ".xlsx":
-		return "excel"
-	case ".ppt", ".pptx":
-		return "powerpoint"
-	case ".zip", ".tar", ".gz", ".7z", "deb":
+	case "rar", ".zip", ".tar", ".gz", ".7z", "deb":
 		return "archive"
 	case ".mp3", ".wav", ".flac":
 		return "audio"
-	case ".mp4", ".avi", ".mkv":
-		return "video"
-	case ".jpg", ".jpeg", ".png", ".gif":
+	case ".webp",".jpg", ".jpeg", ".png", ".gif":
 		return "image"
-	case ".py":
-		return "python"
-	case ".js":
-		return "javascript"
-	case ".html", ".htm":
-		return "html"
-	case ".css":
-		return "css"
+	case ".crdownload":
+		return "crd"
 	default:
 		return "other"
 	}
