b96bba096715feb363e336f934240d9f25cd8fb0
johnotieno0 <poptiff@gmail.com>
2024-11-11 23:30:04 +0300

    update: test for colorName function

---
+++ lsfunctions/color_test.go
@@ -0,0 +1,26 @@
+package lsfunctions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_colorName(t *testing.T) {
+	type args struct {
+		entry Entry
+	}
+	tests := []struct {
+		name string
+		args args
+		want Entry
+	}{
+		{name: "test 1", args: args{entry: Entry{Name: "test.txt", Mode: "-rw-r--r--"}}, want: Entry{Name: "test.txt", Mode: "-rw-r--r--"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := colorName(tt.args.entry); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("colorName() = %q, want %q", got.Name, tt.want.Name)
+			}
+		})
+	}
+}
