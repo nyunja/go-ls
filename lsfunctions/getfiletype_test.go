a7ca07ef12081f37bd35b3948b0dd63950130491
johnotieno0 <poptiff@gmail.com>
2024-11-14 16:19:21 +0300

    update: getFileType return values in the test

--- lsfunctions/getfiletype_test.go
+++ lsfunctions/getfiletype_test.go
@@ -15,7 +15,7 @@ func Test_getFileType(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getFileType(tt.args.entry); got != tt.want {
+			if _, got := getFileType(tt.args.entry); got != tt.want {
 				t.Errorf("getFileType() = %v, want %v", got, tt.want)
 			}
 		})
