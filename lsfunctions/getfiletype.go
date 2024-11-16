84c816e95edda8d0fe23fccee59eaf44893fde01
johnotieno0 <poptiff@gmail.com>
2024-11-17 00:52:35 +0300

    refactor: use new format permissions to get type

--- lsfunctions/getfiletype.go
+++ lsfunctions/getfiletype.go
@@ -7,37 +7,30 @@ import (
 func getFileType(entry Entry) (Entry, string) {
 	mod := entry.Mode
 	// Handle setuid files
-	if entry.Mode[0] == 'u' {
-		entry.Mode = swapU(entry.Mode)
+	if entry.Mode[3] == 's' ||  entry.Mode[3] == 'S' {
 		return entry, "setuid"
 	}
 	// Handle setuid files
-	if entry.Mode[0] == 'g' {
-		entry.Mode = swapG(entry.Mode)
+	if entry.Mode[6] == 's' ||  entry.Mode[6] == 'S' {
 		return entry, "setgid"
 	}
 	// Handle directories
 	if entry.Mode[0] == 'd' {
-		if strings.Contains(entry.Mode, "t") {
-			entry.Mode = swapT(entry.Mode)
+		if strings.HasSuffix(entry.Mode, "t") || strings.HasSuffix(entry.Mode, "T") {
+			return entry, "sticky"
 		}
 		return entry, "dir"
 	}
 	// Handle device files
-	if entry.Mode[0] == 'D' {
-		entry.Mode = "b" + strings.TrimPrefix(entry.Mode, "D")
+	if entry.Mode[0] == 'b' || entry.Mode[0] == 'c' {
 		return entry, "dev"
 	}
 
-	// Ensure mode is properly formatted
-	if len(entry.Mode) != 10 {
-		entry.Mode = "b" + entry.Mode
-		if len(entry.Mode) != 10 {
-			entry.Mode += "-"
-		}
-	}
 	if strings.ContainsAny(mod, "x") {
 		return entry, "exec"
+	}
+	if strings.Count(mod, "w") == 3 {
+		return entry, "worldw-ritable"
 	}
 	name := strings.Trim(entry.Name, "'")
 	tokens := strings.Split(strings.ToLower(name), ".")
@@ -49,7 +42,7 @@ func getFileType(entry Entry) (Entry, string) {
 		return entry, "text"
 	case ".pdf":
 		return entry, "pdf"
-	case "rar", ".zip", ".tar", ".gz", ".7z", "deb":
+	case ".rar", ".zip", ".tar", ".gz", ".7z", "deb":
 		return entry, "archive"
 	case ".mp3", ".wav", ".flac":
 		return entry, "audio"
@@ -60,22 +53,4 @@ func getFileType(entry Entry) (Entry, string) {
 	default:
 		return entry, "other"
 	}
-}
-
-func swapG(s string) string {
-	s = strings.Replace(s, "g", "-", 1)
-	res := []rune{}
-	count := 0
-	for _, ch := range s {
-		if ch == 'x' {
-			count++
-		}
-		if count == 2 {
-			res = append(res, 's')
-			count++
-		} else {
-			res = append(res, ch)
-		}
-	}
-	return string(res)
 }
