1aea42c825ad1b0d1262f8cabb7486e23ec96969
johnotieno0 <poptiff@gmail.com>
2024-11-19 07:21:59 +0300

    refactor: DisplayLongFormat to call getWidths

--- lsfunctions/longlist.go
+++ lsfunctions/longlist.go
@@ -3,43 +3,25 @@ package lsfunctions
 import (
 	"fmt"
 	"io"
-	"os"
-	"strings"
 )
 
-// var specialFiles = map[string]string{
-// 	"../lib/snapd/snap-confine": "setuid",
-// 	"ssh_agent":                 "setgid",
-// 	"umount":                    "setuid",
-// 	"su":                        "setuid",
-// 	"sudo":                      "setuid",
-// 	"passwd":                    "setuid",
-// 	"gpasswd":                   "setuid",
-// 	"fusermount3":               "setuid",
-// 	"newgrp":                    "setgid",
-// 	"mount":                     "setuid",
-// 	"newuidmap":                 "setuid",
-// 	"newgidmap":                 "setuid",
-// 	"umount2":                   "setuid",
-// 	"expiry":                    "setgid",
-// 	"chsh":                      "setuid",
-// 	"chfn":                      "setuid",
-// 	"chage":                     "setgid",
-// 	"/proc/self/fd":             "dir",
-// }
-
+// DisplayLongFormat displays the entries in long format.
 func DisplayLongFormat(w io.Writer, entries []FileDetails) {
 	t := getTotalBlocks(entries)
 	if ShowTotals {
 		fmt.Printf("total %d\n", t)
 	}
-	newEntries, widths := processEntries(entries)
+	newEntries := generateEntries(entries)
+	widths := getWidths(newEntries)
 	for _, entry := range newEntries {
-		fmt.Fprintln(w, GetLongFormatString2(entry, widths))
+		fmt.Fprintln(w, getLongFormatString(entry, widths))
 	}
 }
 
-func GetLongFormatString2(e Entry, w Widths) string {
+// getLongFormatString returns the long format string with the given entry and widths.
+// The width of each column is determined by the maximum width of the corresponding column in the input entries.
+// If the entry is a symbolic link, the link target is also displayed in color.
+func getLongFormatString(e Entry, w Widths) string {
 	e = colorName(e, false)
 	s := ""
 	if w.minorCol == 0 {
@@ -51,56 +33,4 @@ func GetLongFormatString2(e Entry, w Widths) string {
 		s += " -> " + colorLinkTarget(e.Path, e.LinkTarget)
 	}
 	return s
-}
-
-func colorLinkTarget(path, s string) string {
-	absPath := resolveRelativePath(path, s)
-	// Handle shared folder link targets
-	if strings.HasPrefix(s, "../share") {
-		absPath = "/usr" + absPath
-	}
-	info, err := os.Lstat(absPath)
-	var newEntry Entry
-	if os.IsNotExist(err) {
-		return s
-	} else if err != nil {
-		return s
-	} else {
-		permissions, err := formatPermissionsWithACL(absPath, info.Mode())
-		if err != nil {
-			return "cannot format permissions: " + err.Error()
-		}
-		newEntry = Entry{Name: s, Mode: permissions, Path: absPath}
-	}
-	colorEntry := colorName(newEntry, true)
-
-	return colorEntry.Name
-}
-
-func resolveRelativePath(linkPath, target string) string {
-	linkDir := ""
-	lastSlash := strings.LastIndex(linkPath, "/")
-	if lastSlash != -1 {
-		linkDir = linkPath[:lastSlash]
-	}
-	// Return path as is if it's already absolute
-	if strings.HasPrefix(target, "/") {
-		return target
-	}
-	joinedPath := joinPath(linkDir, target)
-
-	// Normalize combined path
-	segments := strings.Split(joinedPath, "/")
-	var stack []string
-
-	for _, segment := range segments {
-		if segment == ".." {
-			if len(stack) > 0 {
-				stack = stack[:len(stack)-1]
-			}
-		} else if segment != "." && segment != "" {
-			stack = append(stack, segment)
-		}
-	}
-	return "/" + strings.Join(stack, "/")
 }
