6f7d83d13da858838d5928d6ee1218f079db673a
johnotieno0 <poptiff@gmail.com>
2024-11-20 09:03:45 +0300

    fix: correct display of paths

--- main.go
+++ main.go
@@ -21,16 +21,33 @@ func main() {
 	paths, idx := ls.SortPaths(paths)
 
 	for i, path := range paths {
-		if i >= idx {
-			if flags.Recursive || len(paths) >= 1 {
-				if i > 0 {
-					fmt.Println()
-				}
-				if len(paths) > 1 {
-					fmt.Printf("%s:\n", path)
-				}
+		if flags.Recursive {
+			if flags.Long {
+				ls.ShowTotals = true
 			}
-			ls.ShowTotals = true
+			if i > 0 {
+				fmt.Println()
+			}
+			fmt.Printf("%s:\n", path)
+		} else if i >= idx {
+			if flags.Long {
+				ls.ShowTotals = true
+			}
+			if i > 0 {
+				fmt.Println()
+			}
+			if len(paths) > 1 {
+				fmt.Printf("%s:\n", path)
+			}
+			// if flags.Recursive || len(paths) >= 1 {
+			// 	if i > 0 {
+			// 		fmt.Println()
+			// 	}
+			// 	if len(paths) > 1 {
+			// 		fmt.Printf("%s:\n", path)
+			// 	}
+			// }
+			// ls.ShowTotals = true
 		}
 		err := ls.ListPath(path, flags)
 		if err != nil {
